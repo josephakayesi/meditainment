52ea8e9c46ece9ace0daabbf45e08ca8ba0766ae
Joseph Akayesi <josephakayesi@gmail.com>
2024-01-30 21:54:44 +0000

    [feat]: simulates email subscription and adds notification

--- src/app/ui/NewsletterCard.tsx
+++ src/app/ui/NewsletterCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
 import Image from "next/image";
 
 import { bricolageGrotesque, uncutSans } from "@/app/ui/fonts";
@@ -8,23 +9,56 @@ import { bricolageGrotesque, uncutSans } from "@/app/ui/fonts";
 import giftIcon from "../../../public//icons/gift.svg";
 import arrowRight from "../../../public/icons/arrow-right.svg";
 
+const mockNewsletterSubscription = (): Promise<{
+  success: boolean;
+  message: string;
+}> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const response = { success: true, message: "Thank you for subscribing!" };
+      resolve(response);
+    }, 2000);
+  });
+};
+
+const submitButtonLoading = () => {
+  return (
+    <button type="submit" className="bg-[#02020240] rounded-full p-8" disabled>
+      <div className="flex md:w-[1.5rem] md:h-[1.5rem] w-[1.5rem] h-full items-center justify-center text-center">
+        <Image src={arrowRight} width={14} height={14} alt={"arrow right"} />
+      </div>
+    </button>
+  );
+};
 function NewsletterCard() {
+  const [submitLoading, setSubmitLoading] = useState<boolean>(false);
+
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const formData = new FormData(event.currentTarget);
+    setSubmitLoading(true);
 
-    const response = await fetch("/api/newsletter", {
-      method: "POST",
-      body: formData,
-    });
-
-    const data = await response.json();
-
-    console.log(data);
+    try {
+      const response = await mockNewsletterSubscription();
+      enqueueSnackbar(response.message, {
+        variant: response.success ? "success" : "error",
+        hideIconVariant: false,
+      });
+    } catch (error) {
+      setSubmitLoading(false);
+    } finally {
+      setSubmitLoading(false);
+    }
   }
+
   return (
     <div className="flex flex-col bg-[#FCF5E5] p-[2rem] gap-8 rounded-sm w-full md:w-full lg:w-[28.5rem]">
+      <SnackbarProvider
+        classes={{
+          containerRoot: `${uncutSans.variable} font-mono font-normal`,
+        }}
+      />
+
       <Image src={giftIcon} width={24} height={24} alt="gift icon" />
       <div className="space-y-4">
         <div
@@ -44,21 +78,27 @@ function NewsletterCard() {
         className="flex gap-[0.25rem] justify-between w-full"
       >
         <input
-          type="text"
+          type="email"
+          required
           name="name"
-          className={`p-8 rounded-full border border-[#020202] w-full lg:w-[18.75rem] ${uncutSans.variable} text-[1rem] font-mono font-normal`}
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          className={`p-8 rounded-full border border-[#020202] w-full lg:w-[18.75rem] ${uncutSans.variable} text-[1rem] font-mono font-normal invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500`}
           placeholder="Enter your email"
         />
-        <button type="submit" className="bg-[#020202] rounded-full p-8">
-          <div className="flex md:w-[1.5rem] md:h-[1.5rem] w-[1.5rem] h-full items-center justify-center text-center">
-            <Image
-              src={arrowRight}
-              width={14}
-              height={14}
-              alt={"arrow right"}
-            />
-          </div>
-        </button>
+        {submitLoading ? (
+          submitButtonLoading()
+        ) : (
+          <button type="submit" className="bg-[#020202] rounded-full p-8">
+            <div className="flex md:w-[1.5rem] md:h-[1.5rem] w-[1.5rem] h-full items-center justify-center text-center">
+              <Image
+                src={arrowRight}
+                width={14}
+                height={14}
+                alt={"arrow right"}
+              />
+            </div>
+          </button>
+        )}
       </form>
     </div>
   );
